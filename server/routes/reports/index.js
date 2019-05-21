b496ece920c1b53f87ae0f57e95f5d6ad90d8596
Warren Ayling <warren.ayling@ext.soprasteria.com>
2019-05-21 09:58:16 +0100

    Adding a temp route to get user set.

--- server/routes/reports/index.js
+++ server/routes/reports/index.js
@@ -4,9 +4,11 @@ const router = express.Router();
 
 const WDF = require('./wdf');
 const DailySnapshot = require('./dailySnapshot');
+const TempUsers = require('./tempUsers');
 
 router.use('/wdf', WDF);
 router.use('/dailySnapshot', DailySnapshot);
+router.use('/users', TempUsers);
 
 router.route('/').get(async (req, res) => {
     return res.status(501).send();
